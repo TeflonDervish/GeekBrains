92c833b7986a6d563861732f2e3d6e85fbb17dfe
Teflon Dervish <voltvilty45@gmail.com>
2024-03-02 23:03:48 +0500

    Add java doc )))

--- src/OOP/task5/View/ViewClassEng.java
+++ src/OOP/task5/View/ViewClassEng.java
@@ -7,21 +7,43 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 
+import java.util.Collection;
+import java.util.Scanner;
+
+/**
+ * Реализация интерфейса {@code iGetView} для отображения информации и взаимодействия с пользователями на английском языке.
+ */
 public class ViewClassEng implements iGetView {
+
+    /**
+     * Выводит детали всех студентов из предоставленной коллекции.
+     *
+     * @param students Коллекция объектов Student, которые будут отображены.
+     */
     @Override
     public void printAllStudents(Collection<Student> students) {
+        System.out.println("_____Список студентов_____");
 
-        System.out.println("_____Student list_____");
-
+        // Выводит детали каждого студента в коллекции
         System.out.println("students = " + students);
 
         System.out.println("______________________");
     }
 
+    /**
+     * Выводит сообщение пользователю и ожидает ввода пользователя.
+     *
+     * @param msg Сообщение, которое будет отображено пользователю.
+     * @return Строка, содержащая ввод пользователя.
+     */
     @Override
     public String prompt(String msg) {
         Scanner in = new Scanner(System.in);
+
+        // Отображает сообщение пользователю
         System.out.println(msg);
+
+        // Считывает и возвращает ввод пользователя
         return in.nextLine();
     }
 }
